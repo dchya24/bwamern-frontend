162b3762178bb1e5aab67b567ee0c10fa1437021
Cahya Dinar Prastyo <cahyadinar241@gmail.com>
2020-05-10 13:39:45 +0700

    first commit

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import Button from "./index"
+import { BrowSerRouter as Router } from "react-router-dom"
+
+test("Should not allowed click button if Disabled is present", () => {
+    const { container } = render(<Button isDisabled></Button>)
+
+    expect(container.querySelector('span.disabled')).toBeInTheDocument()
+})
+
+test("Should render loading.../spinner", () => {
+  const { container,getByText } = render(<Button isLoading></Button>)
+
+  expect(getByText(/loading/i)).toBeInTheDocument()
+  expect(container.querySelector('span')).toBeInTheDocument()
+})
+
+test("Should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>)
+
+  expect(container.querySelector('a')).toBeInTheDocument()
+})
---
+++ src/parts/iconText.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Button from "elements/Button"
+
+export default function IconText(){
+  return(
+    <Button className="brand-text-icon" type="link" href="">
+      Stay<span class="text-gray-800">cation.</span>
+    </Button>
+  )
+}
